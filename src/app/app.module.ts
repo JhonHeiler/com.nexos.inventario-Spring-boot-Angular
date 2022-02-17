8bf5410b404d817869ccdc2710e91a4cf0d756e6
main <heylerty7@gmail.com>
2022-02-17 00:22:59 -0500

    client-

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './component/add-product/add-product.component';
+import { EditProductComponent } from './component/edit-product/edit-product.component';
+import { ListProductComponent } from './component/list-product/list-product.component';
+import { ListHistoryComponent } from './component/list-history/list-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    EditProductComponent,
+    ListProductComponent,
+    ListHistoryComponent
   ],
   imports: [
     BrowserModule,
